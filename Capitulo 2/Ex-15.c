8400e444918453285ec421b6b20d9ecda871e369
Breno Silva <114250412+Breno-S@users.noreply.github.com>
2024-02-26 17:27:31 -0300

    Capítulo 2

--- Capitulo 2/Ex-15.c
+++ Capitulo 2/Ex-15.c
@@ -4,11 +4,9 @@ tabulação horizontal e uma em cada linha. Use um único comando printf() para
 cada operação de escrita das três variáveis. */
 
 /*
-
 Teste diversos valores para cada uma das variáveis, separando com espaços,
 ENTERs, sem separar, invertendo a ordem dos valores, omitindo valores; para
 entender o comportamento das funções de scanf e printf.
-
 */
 
 #include <stdio.h>
